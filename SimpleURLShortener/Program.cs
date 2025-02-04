3b62dc5a8e5ce2c46fa68414243d357b3ef9f410
Ravikumar Makwana <17comp.ravi.makwana@gmail.com>
2025-02-04 20:59:54 +0530

    Add project files.

---
+++ SimpleURLShortener/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        URLShortener uRLShortener = new URLShortener();
+        string longURL1 = "https://github.com/ravikumarmakwana/DesignPatterns/blob/master/DesignPatterns/Creational/Singleton/Singleton1.cs";
+        string longURL2 = "https://github.com/ravikumarmakwana/DesignPatterns/blob/master/DesignPatterns/Creational/Builder/CarBuilder.cs";
+        string shortenURL1 = uRLShortener.CreateShorten(longURL1);
+        string shortenURL2 = uRLShortener.CreateShorten(longURL2);
+
+        Console.WriteLine(longURL1);
+        Console.WriteLine(shortenURL1);
+
+        Console.WriteLine("##############################");
+
+        Console.WriteLine(longURL2);
+        Console.WriteLine(shortenURL2);
+
+        Console.WriteLine("################################");
+
+        Console.WriteLine(uRLShortener.Access(shortenURL1));
+        Console.WriteLine(uRLShortener.Access(shortenURL2));
+    }
+}
---
+++ SimpleURLShortener/URLShortener.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+public class URLShortener
+{
+    private static readonly Dictionary<string, string> _urlDictionary = new();
+    private static long _counter = 100000;
+    private const string _base62Char = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private static string EncodeBase62(long num)
+    {
+        StringBuilder sb = new();
+        while (num > 0)
+        {
+            sb.Append(_base62Char[(int)(num % 62)]);
+            num /= 62;
+        }
+        return sb.ToString();
+    }
+
+    public string CreateShorten(string longURL)
+    {
+        string shortenURL = $"https://shorten.com/{EncodeBase62(_counter)}";
+        _counter++;
+
+        _urlDictionary[shortenURL] = longURL;
+        return shortenURL;
+    }
+
+    public string Access(string shortenURL)
+    {
+        if (_urlDictionary.ContainsKey(shortenURL))
+        {
+            return _urlDictionary[shortenURL];
+        }
+        throw new InvalidOperationException("Given Shorten URL expired or invalid.");
+    }
+
+}
